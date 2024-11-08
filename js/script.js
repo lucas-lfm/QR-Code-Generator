8abfa92287484bf20901ce0029eb857a00ea2d00
lucas-lfm <mendes.lucas.lfm@gmail.com>
2024-11-08 11:25:04 -0300

    feat: add download qr-code button

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
 const container = document.querySelector(".container"),
   qrInput = container.querySelector(".form input"),
   qrCodeArea = container.querySelector(".qr-code"),
-  generateBtn = container.querySelector(".form button");
+  generateBtn = container.querySelector(".form button"),
+  downloadBtn = document.createElement("button");
 
 let preValue;
 let qrcode;
+
+downloadBtn.innerText = "Baixar QR Code";
+downloadBtn.style.display = "none";
+container.appendChild(downloadBtn);
 
 const makeCode = () => {
   let qrValue = qrInput.value.trim();
@@ -28,15 +33,28 @@ const makeCode = () => {
     });
 
     generateBtn.innerText = "Gerar QR Code";
+    downloadBtn.style.display = "inline";
   }, 1000);
 };
 
-generateBtn.addEventListener("click", () => {
-  makeCode();
-});
+const downloadQRCode = () => {
+  const qrCanvas = qrCodeArea.querySelector("canvas");
+  if (qrCanvas) {
+    const qrDataUrl = qrCanvas.toDataURL("image/png");
+
+    const link = document.createElement("a");
+    link.href = qrDataUrl;
+    link.download = "qrcode.png";
+    link.click();
+  }
+};
+
+generateBtn.addEventListener("click", makeCode);
 
 qrInput.addEventListener("keydown", (e) => {
   if (e.keyCode == 13) {
     makeCode();
   }
 });
+
+downloadBtn.addEventListener("click", downloadQRCode);
